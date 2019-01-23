3c4d165594d59e4bbd6086e39aff4ce03719bd00
shuandr <shuandr@gmail.com>
2019-01-23 12:07:56 +0200

    new breakfasts

--- assets/js_plugins/myAngular.js
+++ assets/js_plugins/myAngular.js
@@ -23,12 +23,12 @@ app.controller('menuCtrl', function($scope, $http) {
 
     });
 
-    $http.get("assets/json/brkfst.json").then(function(response) {
+    $http.get("assets/json/brkfst_2.json").then(function(response) {
         $scope.brkfst = response.data;
 
     });
 
-    $http.get("assets/json/brkfst_add.json").then(function(response) {
+    $http.get("assets/json/brkfst_add_2.json").then(function(response) {
         $scope.brkfstAdd = response.data;
 
     });
